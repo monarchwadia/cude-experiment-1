297220f49ae64f8a8f072060b14d9f31c524b51e
Monarch Wadia <monarchwadia@gmail.com>
2024-03-23 06:01:48 -0400

    Initial Commit

---
+++ main.cu
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// CUDA Kernel function to add two numbers
+__global__ void add(int a, int b, int *c)
+{
+    *c = a + b;
+}
+
+int main()
+{
+    int a, b, c; // Host copies of a, b, c
+    int *d_c;    // Device copy of c
+    int size = sizeof(int);
+
+    // Setup input values
+    a = 2;
+    b = 7;
+
+    // Allocate space for device copy of c
+    cudaMalloc((void **)&d_c, size);
+
+    // Launch add() kernel on GPU with one thread
+    add<<<1, 1>>>(a, b, d_c);
+
+    // Copy result back to host
+    cudaMemcpy(&c, d_c, size, cudaMemcpyDeviceToHost);
+
+    // Cleanup
+    cudaFree(d_c);
+
+    printf("Result: %d + %d = %d\n", a, b, c);
+    return 0;
+}
